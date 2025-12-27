842badb8a9d5c47e0874170a9739c31248f2a641
julius caezar <julius.caezar25@gmail.com>
2025-12-26 23:40:28 -0300

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,21 @@
+import prisma from "@memora/db";
+import { env } from "@memora/env/server";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql",
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+  },
+});
