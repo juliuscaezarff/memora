a882a63312c4a05329a9d0546aa7a7ad95a64a06
julius caezar <julius.caezar25@gmail.com>
2025-12-29 12:52:19 -0300

    fix: fuck prod

--- apps/server/tsdown.config.ts
+++ apps/server/tsdown.config.ts
@@ -5,15 +5,5 @@ export default defineConfig({
   format: "esm",
   outDir: "./dist",
   clean: true,
-  noExternal: [
-    "@memora/api",
-    "@memora/auth",
-    "@memora/db",
-    "@memora/env",
-    /@orpc\/.*/,
-    "hono",
-    "better-auth",
-  ],
-  external: ["@prisma/client"],
-  treeshake: true,
+  noExternal: [/@memora\/.*/],
 });
